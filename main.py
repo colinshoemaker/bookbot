131f02304256fd3d2a989837c8d471cd798cd6e9
colinshoemaker <colinjkshoemaker@gmail.com>
2025-01-22 12:12:34 -0700

    func to create dict of number of ea letter

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-
 def main():
     path = "books/frankenstein.txt"
     text = getbook(path) 
     numwords = count_words(text)
     print(numwords)
+    print(count_characters(text))
     
 def getbook(path):
     with open(path) as f:
         file_contents = f.read()
-
     return file_contents
     
 def count_words(file_contents):
-        words = file_contents.split()
-        numwords = len(words)
-        return numwords
+    words = file_contents.split() 
+    return len(words)
 
+def count_characters(text):
+    lowtext = text.lower()
+    lettd ={}
+    for let in lowtext:
+        if let in lettd:
+            lettd[let] += 1
+        else:
+            lettd[let]=1
+    return lettd
+    
     
 main()
